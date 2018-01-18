da42e4138358e24af201013776c218443072e2b1
ParkHongGyen <35034324+ParkHongGyen@users.noreply.github.com>
2018-01-18 22:00:43 +0900

    papago nmt 번역 완성(이전오류:이름겹쳐서,url수정,Gson이용)

--- app/src/main/java/com/example/phg/getjob/MainActivity.java
+++ app/src/main/java/com/example/phg/getjob/MainActivity.java
@@ -5,35 +5,41 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends TabActivity {
 
-    Button b1;
-    EditText et1;
-    TextView tv1;
-    NaverSearchTask task;
 
+    TextView tvresult;//번역창
+    EditText ettrans;//입력창
+    Button bttranslate;//번역 버튼
 
+    Button sb; //채용정보 검색 버튼튼
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        b1 = (Button) findViewById(R.id.button1);
-        et1 = (EditText) findViewById(R.id.editText1);
-        tv1 = (TextView) findViewById(R.id.textView1);
-
 
 
 //////////////////////////////////////////////////////////////////////tab host
@@ -57,7 +63,7 @@ public class MainActivity extends TabActivity {
 
         tabHost.setCurrentTab(0);
         ////////////////////////////////////////////////////////////////
-        ////////////////////////////////////////////////////////////db
+        ////////////////////////////////////////////////////////////DB
         final dbHelper dbHelp = new dbHelper(getApplicationContext(), "LIKE.db", null, 1);//db조건
 
         final EditText etlike = (EditText)findViewById(R.id.etLike);//참조
@@ -85,13 +91,116 @@ public class MainActivity extends TabActivity {
             }
         });
 ////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////네이버 사전
+
+        tvresult = (TextView)findViewById(R.id.tv_result);
+        ettrans = (EditText)findViewById(R.id.etsource);
+        bttranslate = (Button) findViewById(R.id.bt_translate);
 
 
 
+        bttranslate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ettrans.getText().toString().length()==0){
+                    Toast.makeText(MainActivity.this, "내용을 입력하세요." ,Toast.LENGTH_SHORT).show();
+                    ettrans.requestFocus();
+                    return;
+                }
+                NaverTranslateTask asyncTask = new NaverTranslateTask();
+                String sText = ettrans.getText().toString();
+                asyncTask.execute(sText);
 
-    }
-///////////////////////////////////////////////////////////////////////
+
+
+            }
+        });
+
+
+    } ////////oncreate
+
+   public class NaverTranslateTask extends AsyncTask<String,Void,String>
+   {
+       public String resultText;
+       String clientId = "qPRWHkJcumXLFGtvzWo2";//애플리케이션 클라이언트 아이디값";
+       String clientSecret = "yjQBWLhQ50";//애플리케이션 클라이언트 시크릿값";
+       String sourceLang = "en";
+       String targetLang = "ko";
+
+       @Override
+       protected void onPreExecute() {
+           super.onPreExecute();
+       }
+
+       @Override
+       protected String doInBackground(String... strings) {
+           String sourceText = strings[0];
+           try {
+               String text = URLEncoder.encode(sourceText, "UTF-8");
+               String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+               URL url = new URL(apiURL);
+               HttpURLConnection con = (HttpURLConnection)url.openConnection();
+               con.setRequestMethod("POST");
+               con.setRequestProperty("X-Naver-Client-Id", clientId);
+               con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+               // post request
+               String postParams = "source="+sourceLang+"&target="+targetLang+"&text=" + text;
+               con.setDoOutput(true);
+               DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+               wr.writeBytes(postParams);
+               wr.flush();
+               wr.close();
+               int responseCode = con.getResponseCode();
+               BufferedReader br;
+               if(responseCode==200) { // 정상 호출
+                   br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+               } else {  // 에러 발생
+                   br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+               }
+               String inputLine;
+               StringBuffer response = new StringBuffer();
+               while ((inputLine = br.readLine()) != null) {
+                   response.append(inputLine);
+               }
+               br.close();
+               //System.out.println(response.toString());
+               return response.toString();
+
+           } catch (Exception e) {
+               //System.out.println(e);
+               Log.d("error", e.getMessage());
+               return null;
+           }
+       }
+//번역된 결과를 받아서 처리 부분
+       @Override
+       protected void onPostExecute(String s) {
+           super.onPostExecute(s);
+
+           Gson gson = new GsonBuilder().create();
+           JsonParser parser = new JsonParser();
+           JsonElement rootObj = parser.parse(s.toString())
+                   //원하는 데이터 까지 찾아 들어간다.
+                   .getAsJsonObject().get("message")
+                   .getAsJsonObject().get("result");
+           //안드로이드 객체에 담기
+           TranslatedItem items = gson.fromJson(rootObj.toString(), TranslatedItem.class);
+           //Log.d("result", items.getTranslatedText());
+           //번역결과를 텍스트뷰에 넣는다.
+           tvresult.setText(items.getTranslatedText());
+       }
+
+       private class TranslatedItem{
+           String translatedText;
+
+           public String getTranslatedText(){
+               return translatedText;
+           }
+       }
+   }
+
+
+
+    ///////////////////////////////////////////////////////////////////////페이지 버튼
     public void pnggo1(View v) //정보처리기사기출 이벤트
     {
         Uri uri = Uri.parse("http://www.gunsys.com/cbt_list/index.php?cbt=gisa");//인터넷주소
@@ -107,27 +216,36 @@ public class MainActivity extends TabActivity {
 
 //////////////////////////////////////////////////saramin액티비티
     public void saramingo(View v){
+        final RadioGroup rg = (RadioGroup)findViewById(R.id.radiogroup1);
+        sb = (Button)findViewById(R.id.searchb);
+        int id = rg.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton) findViewById(id);
+        String result = rb.getText().toString();
+
         Intent it = new Intent(getApplicationContext(), SaraminActivity.class);
+
+        it.putExtra("value", " result");
         startActivity(it);
     }
 
     /////////////////////////////////////////////////////////////////////네이버사전
 
 
-    class  NaverSearchTask  extends AsyncTask<String, Void, String>
+    /*class  NaverSearchTask  extends AsyncTask<String, Void, String>
     {
         @Override
         protected String doInBackground(String... strings) {
+
             String clientId = "qPRWHkJcumXLFGtvzWo2";//애플리케이션 클라이언트 아이디값";
             String clientSecret = "yjQBWLhQ50";//애플리케이션 클라이언트 시크릿값";
             String url = "https://openapi.naver.com/v1/papago/n2mt";
             XmlPullParserFactory factory;
             XmlPullParser parser;
-            URL xmlUrl;
+            URL xmlUrl = new URL(url);
             String returnResult = "";
             try{
                 boolean flag1 = false;
-                xmlUrl = new URL(url);
+
                 xmlUrl.openConnection().getInputStream();
                 factory = XmlPullParserFactory.newInstance();
                 parser = factory.newPullParser();
@@ -172,8 +290,10 @@ public class MainActivity extends TabActivity {
         task = new NaverSearchTask();
         task.execute(et1.getText().toString());
         et1.setText("");
-    }
+    }*/
 //////////////////////////////////////////////////
+
+
 
 
 }
